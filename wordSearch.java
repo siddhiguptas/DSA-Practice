5b4eb1c1ffa14b2abf1b4846bd2f37c02d5381c3
Siddhi905 <siddhigupta811@gmail.com>
2025-03-09 21:00:54 +0530

    first commit

---
+++ wordSearch.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, 0, i, j, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int index, int i, int j, boolean[][] visited) {
+        if (index == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        visited[i][j] = true;
+        boolean found = dfs(board, word, index + 1, i + 1, j, visited) ||
+                        dfs(board, word, index + 1, i - 1, j, visited) ||
+                        dfs(board, word, index + 1, i, j + 1, visited) ||
+                        dfs(board, word, index + 1, i, j - 1, visited);
+        visited[i][j] = false;
+        
+        return found;
+    }
+}
